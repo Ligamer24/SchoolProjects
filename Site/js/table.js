caad9f05eb37a0845922dc416905b672f5f2a650
Ligamer24 <102103876+Ligamer24@users.noreply.github.com>
2022-06-14 12:09:23 +0100

    Add files via upload

---
+++ Site/js/table.js
@@ -0,0 +1,64 @@
+//Alunos e respetivos nomes e textos
+var Carlos = {Nome: "Carlos Cordeiro", TemaDH: "Caso de Homofobia", TemaS: "",TextoS: '',TextoDH: 'Em uma decisão histórica, a Suprema Corte da Índia descriminalizou a homossexualidade no país.<br>A discriminação pela orientação sexual passou a ser uma violação dos direitos fundamentais no país asiático.A decisão unânime revogou uma sentença de 2013 que validava um artigo do Código Penal indiano da era colonial que punia "relações carnais contra a ordem da natureza" e criminalizava com penas de 10 anos de prisão as relações entre pessoas do mesmo sexo.No entanto, em dezenas de países, gays, lésbicas e transexuais ainda são tratados como criminosos, e estão sujeitos até à pena de morte em alguns deles. É a chamada “homofobia de Estado”.Em relatório, a associação internacional ILGA (International Lesbian, Gay, Bisexual, Trans and Intersex Association), que monitora as leis relacionadas ao tema, identifica 72 países em que relações entre pessoas do mesmo sexo são consideradas crime. Com a descriminalização aprovada na Índia, portanto, restam ainda 71 países nessa condição.O número representa um terço do total de estados membros da ONU (Organização das Nações Unidas). Em 2006, quando foi feito o primeiro estudo, a lista tinha 92 países.As punições variam de multas e prisão à pena de morte. Há também países que não preveem penalidade ou não a aplicam atualmente, mas mantêm a criminalização em seu código penal.Existem três etapas básicas no caminho para o reconhecimento legal dos direitos LGBT: descriminalização (que relações homossexuais deixem de ser crime), proteção (leis contra a discriminação, por exemplo, no acesso a emprego) e reconhecimento (casamento e direito à adoção, entre outros).O Brasil foi incluído na lista do “reconhecimento” quando o casamento gay foi reconhecido por via judicial.'}
+var Cristiano = {Nome: "Cristiano Coelho", TemaDH: "", TemaS: "", TextoS: '', TextoDH: ''}
+var Daniel = {Nome: "Daniel Carvalhuço", TemaDH: "", TemaS: "", TextoS: '', TextoDH: ''}
+var Duarte = {Nome: "Duarte Reis", TemaDH: "", TemaS: "", TextoS: '', TextoDH: ''}
+var Emanuel = {Nome: "Emanuel Ferreira", TemaDH: "Descriminação dos ciganos", TemaS: "", TextoS: '', TextoDH: 'Como uma das maiores e mais importantes minorias da Europa, os ciganos têm uma longa e extensa história de discriminação. Como tal, continuam a ser um dos grupos mais vulneráveis da Europa. Muitos governos falharam em proteger os ciganos da discriminação e garantir seus amplos direitos humanos, sejam políticos e civis, ou econômicos, sociais e culturais. A discriminação contra os ciganos é flagrante e persistente em várias áreas fundamentais para garantir o respeito pela dignidade humana mínima, como os direitos à educação, habitação, saúde e trabalho. As consequências desta longa história de discriminação resultaram na exclusão social de milhões de ciganos na Europa. No entanto, houve desenvolvimentos positivos: a marginalização dos ciganos na Europa está sendo cada vez mais analisada do ponto de vista dos direitos humanos, e as soluções estão sendo cada vez mais desenvolvidas (seja por ativistas ciganos, ONGs ou por organizações regionais e internacionais). Na UE, a legislação nacional antidiscriminação foi significativamente melhorada com o objetivo de adaptá-la aos requisitos da Diretiva Européia para a Igualdade. Dada a universalidade e indivisibilidade dos direitos humanos, se os direitos de alguém são violados, são os direitos de todos que estão em jogo. Assim, quando os ciganos são discriminados, somos todos discriminados, até porque são cidadãos portugueses como todos os outros. Portanto, os fatores que permitem e agravam a discriminação contra a comunidade cigana local no caso específico do Bairro das Pedreiras devem ser afastados.'}
+var Filipe = {Nome: "Filipe Eiras", TemaDH: "", TemaS: "", TextoS: '', TextoDH: `Neste texto vou falar sobre uma mulher que decidiu fazer um aborto e foi presa por 30 anos por essa causa. O nome da mulher é Esme ela graças ao tratamento médico recebeu um aborto espontâneo e graças a isso n conseguiram salvar o bebé e foi acusada pelo Ministério Publico de ter feito um aborto espontâneo. Durante dois anos da sua vida esteve em prisão preventiva, e por essa causa teve afastada da sua filha de 7 anos até darem inicio ao processo. Em El Salvador, os movimentos feministas lutam há décadas pela reforma do Código Penal. Segundo o El País, a condenação de Esme demonstra a recriminação sofrida pelas mulheres que optam por fazer ou sofrem um aborto. A advogada de Esme, Karla Vaquerano, acusa o juiz de de agir “com parcialidade, assim favorecendo a versão da Procuradoria Geral da República, carregada de estigmas e estereótipos de género”. Vaquerano afirmou que vai recorrer e expressou o agradecimento de Esme às organizações que a têm apoiado neste processo. A revolta no país sofreu um revés no último ano, quando a Assembleia Legislativa decidiu guardar na gaveta uma proposta de reforma do Código Penal, apresentada em 2016 por grupos feministas, que propunha a descriminalização quando a vida da mulher está em risco, em caso de violação ou quando se registam malformações do feto “que inviabilizam a vida fora do útero”. A presidente do Grupo Cidadão pela Descriminalização do Aborto em El Salvador, Morena Herrera, considerou a condenação de Esme “um duro golpe no caminho para o fim da criminalização” de abortos espontâneos, que devem ser “tratados como problemas de saúde pública”.
+Entre 2000 e 2014, pelo menos 49 mulheres foram condenadas em El Salvador por situações relacionadas com o aborto e as autoridades denunciaram 250 mulheres por terem feito um assim dizendo que estsa mulheres em El Salvador não têm os seus direitos do proprio corpo. A condenação de Esme foi a primeira do género em sete anos no país. E apesar do revés, os grupos que lutam pela descriminalização do aborto em El Salvador conquistaram importantes vitórias legais, nomeadamente a libertação de 64 mulheres condenadas por terem sofrido um aborto espontâneo. <br>O El País conta que uma desta mulheres libertadas é Evelyn Hernández, que em 2016 foi detida após entrar em trabalho de parto na casa de banho de casa. O bebé acabou por morrer, no entanto ela nem sabia que estava grávida. Foi condenada a 30 anos de prisão por homicídio qualificado, mas acabou por ser absolvida em 2020 após vários anos de pressão internacional. E assim podemos ver que as mulheres em El Salvador não têm direitos de mandar no seu próprio corpo.`}
+var GonçaloS = {Nome: "Gonçalo Serafim", TemaDH: "", TemaS: "", TextoS: '', TextoDH: ''}
+var GonçaloT = {Nome: "Gonçalo Teófilo", TemaDH: "George Floyd", TemaS: "", TextoS: '', TextoDH: 'O Conselho de Direitos Humanos da ONU aprovou por consenso nesta sexta-feira uma resolução que condena práticas racistas de forças de segurança, nomeando o caso da polícia norte-americana que ocasionou a morte de George Floyd, morto por sufocamento diante de câmeras de telefones por um policia branco.<br> O George Floyd é um caso de Racismo praticado por um policia branco, depois de George Floyd ter dito que nao conseguia respirar o policia continua com o joelho em cima do pescoço do George.<br> O Assasinato aconteceu em Minneapolis no dias 25 de maio de 2020, foi morto durante a abordagem por supostamente usar  uma nota falsa de vinte dólares no supermercado.'}
+var JoaoC = {Nome: "João Correia", TemaDH: "", TemaS: "", TextoS: '', TextoDH: ''}
+var JoaoF = {Nome: "João Fortunato", TemaDH: "", TemaS: "", TextoS: '', TextoDH: ''}
+var Leandro = {Nome: "Leandro Salvador", TemaDH: "", TemaS: "", TextoS: '', TextoDH: ''}
+var Leonardo = {Nome: "Leonardo Augusto", TemaDH: "", TemaS: "Conceito de Sustentabilidade", TextoS: 'Sustentabilidade é um conceito relacionado à conservação ou à manutenção de um cenário no longo prazo, de modo a lidar bem com possíveis ameaças. No aspeto ambiental, em que o termo é empregado com frequência, a sustentabilidade faz referência a um planeta sadio, no qual as pessoas possam encontrar as condições necessárias para a sobrevivência, de geração em geração. A noção de sustentabilidade surgiu baseada no entendimento de que os recursos naturais são finitos. Em relação à biologia, o termo sustentabilidade está ligado à capacidade de regeneração dos ecossistemas diante do uso abusivo dos recursos naturais ou de agressões, como incêndios e eventos naturais, incluindo aí deslizamentos de terra, tsunamis e terremotos.', TextoDH: ''}
+var Rodrigo = {Nome: "Rodrigo Anunciação", TemaDH: "", TemaS: "", TextoS: '', TextoDH: ''}
+var Rui = {Nome: "Rui Zarro", TemaDH: "", TemaS: "", TextoS: '', TextoDH: ''}
+var TomasV = {Nome: "Tomás Vicente", TemaDH: "Casos de Racismo no Futebol", TemaS: "", TextoS: '', TextoDH: '14 de fevereiro de 2020: O Cagliari baniu de forma permanente três adeptos do estádio, identificados por comportamentos racistas e insultos aos jogadores adversários em encontros da Liga italiana.<br> 27 de janeiro de 2020: O Espanyol identificou 12 adeptos envolvidos na imitação de sons de macaco ao avançado basco de origem ganesa Iñaki Williams, quando estava a ser substituído aos 69 minutos do empate caseiro com o Athletic Bilbau (1-1), para a 21.ª jornada do campeonato.<br> 8 de janeiro de 2020: A Lazio foi punida com uma coima de 20 mil euros pelo Conselho de Disciplina da Liga italiana devido às atitudes dos adeptos, que obrigaram à interrupção aos 30 minutos da vitória sobre o Brescia (2-1), da 18.ª jornada, por cânticos racistas entoados na direção do avançado Mario Balotelli.<br> A minha opinião sobre o racismo no futebol é que é extremamente estúpido em pleno 2022 ainda haver casos de racismo. Acho que as pessoas a mandam bocas aos jogadores e a insultá-los deviam ter consequências mais graves. Muitas das pessoas que fazem esses atos saem impunes. Acho que deviam reforçar a vigilância do estádio porque o que essas pessoas não deviam sair livres. Para não haver estes casos, acho que os adeptos das equipas deviam respeitar mais os jogadores porque não fazem ideia dos que eles passam dentro do campo.'}
+var TomasF = {Nome: "Tomás Ferreira", TemaDH: "", TemaS: "", TextoS: '', TextoDH: ''}
+var Lourenço = {Nome: "Lourenço Marques", TemaDH: "", TemaS: "", TextoS: '', TextoDH: ''}
+
+
+
+var alunos = [Carlos, Cristiano, Daniel, Duarte, Emanuel, Filipe, GonçaloS, GonçaloT, JoaoC, JoaoF, Leandro, Leonardo, Rodrigo, Rui, TomasV, 
+TomasF, Lourenço]
+
+
+
+function mostrarTextoSus()
+{
+    var opinioesDiv = document.getElementById("opinioes")
+    var temaDiv = document.getElementById("tema")
+    var autorDiv = document.getElementById("autor")
+    var opiniaoDiv = document.getElementById("opiniao")
+    var texto = "Lorem Ipsum is simply dummy text of the printing and typesetting industryLorem Ipsum has been the industrs standard dummy text ever since the 1500s, hen an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap intoelectronic typesetting, remaining essentially unchanged. It was popularised inthe 1960s with the release of Letraset sheets containing Lorem Ipsum passages,"
+
+    autorDiv.innerHTML = ""
+    temaDiv.innerHTML = "<h1 style='padding: 20px'>Sustentabilidade</h1>"
+    
+    for (var i = 0; i < alunos.length; i++)
+    {
+        
+        n = i + 1
+        autorDiv.innerHTML += "<h2>" + alunos[i].Nome + " Nº" + n + "</h2>" + "<h3>" + alunos[i].TemaS + "</h3>" + "<p>" + alunos[i].TextoS + "</p>"
+    }
+}
+
+function mostrarTextoDH()
+{
+    var opinioesDiv = document.getElementById("opinioes")
+    var temaDiv = document.getElementById("tema")
+    var autorDiv = document.getElementById("autor")
+    var opiniaoDiv = document.getElementById("opiniao")
+    var texto = "Lorem Ipsum is simply dummy text of the printing and typesetting industryLorem Ipsum has been the industrs standard dummy text ever since the 1500s, hen an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap intoelectronic typesetting, remaining essentially unchanged. It was popularised inthe 1960s with the release of Letraset sheets containing Lorem Ipsum passages,"
+
+    autorDiv.innerHTML = ""
+    temaDiv.innerHTML = "<h1 style='padding: 20px'>Direitos Humanos</h1>"
+    
+    for (var i = 0; i < alunos.length; i++)
+    {
+        
+        n = i + 1
+        autorDiv.innerHTML += "<h2>" + alunos[i].Nome + " Nº" + n + "</h2>" + "<h3>" +  alunos[i].TemaDH + "</h3>" + "<p>" + alunos[i].TextoDH + "</p>"
+    }
+}
---
+++ Site/js/textosScript.js
@@ -0,0 +1,19 @@
+
+
+var Carlos = {Nome: "Carlos Cordeiro", Texto: 'Em uma decisão histórica, a Suprema Corte da Índia descriminalizou a homossexualidade no país. A discriminação pela orientação sexual passou a ser uma violação dos direitos fundamentais no país asiático.A decisão unânime revogou uma sentença de 2013 que validava um artigo do Código Penal indiano da era colonial que punia "relações carnais contra a ordem da natureza" e criminalizava com penas de 10 anos de prisão as relações entre pessoas do mesmo sexo.No entanto, em dezenas de países, gays, lésbicas e transexuais ainda são tratados como criminosos, e estão sujeitos até à pena de morte em alguns deles. É a chamada “homofobia de Estado”.Em relatório, a associação internacional ILGA (International Lesbian, Gay, Bisexual, Trans and Intersex Association), que monitora as leis relacionadas ao tema, identifica 72 países em que relações entre pessoas do mesmo sexo são consideradas crime. Com a descriminalização aprovada na Índia, portanto, restam ainda 71 países nessa condição.O número representa um terço do total de estados membros da ONU (Organização das Nações Unidas). Em 2006, quando foi feito o primeiro estudo, a lista tinha 92 países.As punições variam de multas e prisão à pena de morte. Há também países que não preveem penalidade ou não a aplicam atualmente, mas mantêm a criminalização em seu código penal.Existem três etapas básicas no caminho para o reconhecimento legal dos direitos LGBT: descriminalização (que relações homossexuais deixem de ser crime), proteção (leis contra a discriminação, por exemplo, no acesso a emprego) e reconhecimento (casamento e direito à adoção, entre outros).O Brasil foi incluído na lista do “reconhecimento” quando o casamento gay foi reconhecido por via judicial.'}
+var Cristiano = {Nome: "Cristiano Coelho", Texto: ''}
+var Daniel = {Nome: "Daniel Carvalhuço", Texto: ''}
+var Duarte = {Nome: "Duarte Reis", Texto: ''}
+var Emanuel = {Nome: "Emanuel Ferreira", Texto: ''}
+var Filipe = {Nome: "Filipe Eiras", Texto: ''}
+var GonçaloS = {Nome: "Gonçalo Serafim", Texto: ''}
+var GonçaloT = {Nome: "Gonçalo Teófilo", Texto: ''}
+var JoaoC = {Nome: "João Correia", Texto: ''}
+var JoaoF = {Nome: "João Fortunato", Texto: ''}
+var Leandro = {Nome: "Leandro Salvador", Texto: ''}
+var Leonardo = {Nome: "Leonardo Augusto", Texto: ''}
+var Rodrigo = {Nome: "Rodrigo Anunciação", Texto: ''}
+var Rui = {Nome: "Rui Zarro", Texto: ''}
+var TomasV = {Nome: "Tomás Vicente", Texto: ''}
+var TomasF = {Nome: "Tomás Ferreira", Texto: ''}
+var Lourenço = {Nome: "Lourenço Marques", Texto: ''}
